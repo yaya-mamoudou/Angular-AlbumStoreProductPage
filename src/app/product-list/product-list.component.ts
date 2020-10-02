4f0f0d27d93bc57fc721d601dbab169db9a9b6b5
Yaya Mamoudou <yayamamoudou0@gmail.com>
2020-10-02 18:34:40 +0100

    Completed module

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -13,6 +13,7 @@ export class ProductListComponent implements OnInit {
   constructor(private _productService: ProductService) { }
 
   ngOnInit() {
+   this._productService.getProducts().subscribe(response => this.products = response);
   }
 
 }
